f988130f92fcc1ebdb847790771dec541872ec12
houndiusacademy <89109852+houndiusacademy@users.noreply.github.com>
2021-08-18 15:38:54 +0700

    Add files via upload

---
+++ modinfo.lua
@@ -0,0 +1,16 @@
+-- This information tells other players more about the mod
+name = "Gate and Key"
+description = "Spawn 'fancy_fence_gate_door' and 'blue_key' "
+author = "HA"
+version = "1.0" 
+forumthread = ""
+api_version = 10
+dst_compatible = true
+all_clients_require_mod = false 
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+configuration_options =
+{
+}
